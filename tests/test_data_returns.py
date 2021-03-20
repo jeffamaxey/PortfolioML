0b15fd42873d6e8dc9150a8633dbe795f992a4d1
lasala.angelo96@gmail.com <lasala.angelo96@gmail.com>
2021-03-20 16:43:14 +0100

    /test, change doc string

--- tests/test_data_returns.py
+++ tests/test_data_returns.py
@@ -2,21 +2,18 @@
 import unittest
 import sys 
 import pandas as pd
-sys.path.append('/home/angelo/Documenti/PortfolioML/PortfolioML/Data')
+sys.path.append('../PortfolioML/Data')
 import data_returns
 
-dataframe = pd.read_csv('/home/angelo/Documenti/PortfolioML/PortfolioML/Data/PriceData.csv')
+dataframe = pd.read_csv('../PortfolioML/Data/PriceData.csv')
 class TestDataRaturns(unittest.TestCase):
     """Class test for data_returns.py"""
 
     def test_m(self):
-        """test correst resultu with m=1"""
+        """test correst result m"""
         m_list = range(1,10)
         for m in m_list:
             self.assertAlmostEqual(data_returns.get_returns(dataframe,m).ALL[0],(dataframe.ALL[m]/dataframe.ALL[0])-1)
-
-    
-   
 
 if __name__ == '__main__':
     unittest.main()
