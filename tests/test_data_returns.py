566908d35f2d65610d2103f0d7efd91f2d9ea552
RaffaeleParadiso <raffaele05@gmail.com>
2021-03-21 01:20:33 +0100

    another try

--- tests/test_data_returns.py
+++ tests/test_data_returns.py
@@ -2,7 +2,7 @@
 import unittest
 import sys 
 import pandas as pd
-from portfolioML import data_returns
+from portfolioML.data_returns import get_returns
 
 dataframe = pd.read_csv('../portfolioML/data/PriceData.csv')
 class TestDataRaturns(unittest.TestCase):
@@ -12,7 +12,7 @@ class TestDataRaturns(unittest.TestCase):
         """test correst result with some value m"""
         m_list = range(1,10)
         for m in m_list:
-            self.assertAlmostEqual(data_returns.get_returns(dataframe,m).ALL[0],(dataframe.ALL[m]/dataframe.ALL[0])-1)
+            self.assertAlmostEqual(get_returns(dataframe,m).ALL[0],(dataframe.ALL[m]/dataframe.ALL[0])-1)
 
 if __name__ == '__main__':
     unittest.main()
