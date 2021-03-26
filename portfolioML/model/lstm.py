3d8530024ffc6d7e5a4b3175272d308b2798014b
DanieleMDiNosse <danielemdinosse@gmail.com>
2021-03-26 20:00:35 +0100

    X_train and X_test normalization placed at the (maybe) right position

--- portfolioML/model/lstm.py
+++ portfolioML/model/lstm.py
@@ -42,8 +42,11 @@ if __name__ == "__main__":
         T1_target = periods_binary[period]
 
         X_input_train, y_input_train = T1_input[:len_train], T1_target[:len_train]
+        scaler = StandardScaler()
+        X_input_train = scaler.fit_transform(X_input_train) # Normalizzo solo X_train
 
         X_test, y_test = T1_input[len_test:], T1_target[len_test:]
+        X_test = scaler.fit_transform(X_test) # Normalizzo solo X_test
 
         X_train, y_train = get_train_set(X_input_train, y_input_train)
         X_train, y_train = np.array(X_train), np.array(y_train)
@@ -51,16 +54,13 @@ if __name__ == "__main__":
         X_test, y_test = get_train_set(X_test, y_test)
         X_test, y_test = np.array(X_test), np.array(y_test)
 
-        #Normalization and reshaping
-        scaler = StandardScaler()
-        X_train_norm = scaler.fit_transform(X_train)
-        X_train = np.reshape(X_train_norm, (X_train_norm.shape[0], X_train_norm.shape[1], 1))
+        X_train = np.reshape(X_input_train, (X_input_train.shape[0], X_input_train.shape[1], 1))
 
         return X_train, y_train, X_test, y_test
 
     def LSTM_model():
         inputs = Input(shape= (240, 1))
-        hidden = LSTM(2)(inputs)
+        hidden = LSTM(25)(inputs)
         drop = Dropout(0.1)(hidden)
         outputs = Dense(2, activation='softmax')(drop)
 
@@ -71,39 +71,27 @@ if __name__ == "__main__":
     model = LSTM_model()
     print(model.summary())
 
+    #Modello per il primo periodo
     X_train, y_train, X_test, y_test = all_data_LSTM(df_returns, df_binary, 0)
-    history = model.fit(X_train, y_train, epochs=1, batch_size=128, validation_split=0.2, verbose=1)
+    history = model.fit(X_train, y_train, epochs=1000, batch_size=32, validation_split=0.2, verbose=1)
     model.save("LSTM_0_period.h5")
 
-    for i in range(0,3):
-        model = load_model(f"LSTM_{i}_period.h5")
-        X_train, y_train, X_test, y_test = all_data_LSTM(df_returns, df_binary, i+1)
-        history = model.fit(X_train, y_train, epochs=1, batch_size=128, validation_split=0.2, verbose=1)
-        plt.figure()
-        plt.plot(history.history['loss'], label='loss')
-        plt.plot(history.history['val_loss'], label='val_loss')
-        plt.xlabel('Epochs')
-        plt.legend()
-        plt.title(f'Period {i}')
-        plt.grid(True)
-        # #Prediction
-        # y_pred = model.predict(X_test)
-        # for i,j in zip(y_test,y_pred):
-        #     logging.info(i,j)
-        model.save(f"LSTM_{i+1}_period.h5")
-
-
-    # #Loss and Val-loss
-    # print('loss sul traing in funzione delle epoche')
-    # print(history.history['loss'])
-    # print('\t')
-    # print('loss sul validation in funzione delle epoche')
-    # print(history.history['val_loss'])
-    # print('\t')
-
-    # plt.plot(history.history['loss']) #funzione a gomito dalla quale si deve scegliere il valore ottimale delle epoche
-    # plt.plot(history.history['val_loss'])
-    # plt.show()
+    # for i in range(0,3):
+    #     model = load_model(f"LSTM_{i}_period.h5")
+    #     X_train, y_train, X_test, y_test = all_data_LSTM(df_returns, df_binary, i+1)
+    #     history = model.fit(X_train, y_train, epochs=1, batch_size=128, validation_split=0.2, verbose=1)
+    #     plt.figure()
+    #     plt.plot(history.history['loss'], label='loss')
+    #     plt.plot(history.history['val_loss'], label='val_loss')
+    #     plt.xlabel('Epochs')
+    #     plt.legend()
+    #     plt.title(f'Period {i}')
+    #     plt.grid(True)
+    #     # #Prediction
+    #     # y_pred = model.predict(X_test)
+    #     # for i,j in zip(y_test,y_pred):
+    #     #     logging.info(i,j)
+    #     model.save(f"LSTM_{i+1}_period.h5")
 
 
 
