9698e2bf9eb14d44ec74afaa0cb97e418f543b3d
DanieleMDiNosse <danielemdinosse@gmail.com>
2021-03-26 10:52:14 +0100

    starting to construct the real LSTM

--- portfolioML/model/lstm.py
+++ portfolioML/model/lstm.py
@@ -1,14 +1,14 @@
 """LSTM model"""
 import numpy as np
-import pandas as pd 
-import logging 
+import pandas as pd
+import logging
 import argparse
 from keras.layers import Input, Dense, LSTM, Dropout
 from keras.models import Model, Sequential
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath("..")))
-from split import split_sequences
+from split import split_sequences, split_Tperiod
 from portfolioML.data.data_returns import read_filepath
 from sklearn.preprocessing import RobustScaler, StandardScaler
 import matplotlib.pyplot as plt
@@ -29,30 +29,33 @@ if __name__ == "__main__":
               'debug': logging.DEBUG}
 
     logging.basicConfig(level= levels[args.log])
-    
+
     #Read the data
     df_returns = read_filepath(args.returns_file)
     df_binary = read_filepath(args.binary_file)
 
-    #Split data, only one company for the moment. We choose 2/3 of the period for train and 1/3 for test
-    T1_input = df_returns.AEP[:3308]
-    T1_target = df_binary.AEP[:3308]
+    #Split data. We choose 2/3 of the period for train and 1/3 for test
+    len_train = 981
+    len_test = 327
 
-    X_input_train = T1_input[:2900]
-    y_input_train = T1_target[:2900]
+    periods_returns, periods_binary = split_Tperiod(df_returns, df_binary)
 
-    X_test = T1_input[2901:]
-    y_test = T1_target[2901:]
+    T1_input = periods_returns[0]
+    T1_target = periods_binary[0]
+
+    X_input_train = T1_input[:len_train]
+    y_input_train = T1_target[:len_train]
+
+    X_test = T1_input[len_test:]
+    y_test = T1_target[len_test:]
 
     X_train, y_train = split_sequences(X_input_train, y_input_train)
-    print(X_train.shape)
-    
+
     #Normalization and reshaping
     scaler = StandardScaler()
     X_train_norm = scaler.fit_transform(X_train)
     X_train = np.reshape(X_train_norm, (X_train_norm.shape[0], X_train_norm.shape[1], 1))
-    print(X_train.shape)
-    
+
     #Model1
     inputs = Input(shape= (240, 1))
     hidden = LSTM(100)(inputs)
@@ -62,7 +65,7 @@ if __name__ == "__main__":
     model = Model(inputs=inputs, outputs=outputs)
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     print(model.summary())
-    
+
     #Fit
     history = model.fit(X_train, y_train, epochs=20, batch_size=32, validation_split=0.2, verbose=1)
 
@@ -72,12 +75,12 @@ if __name__ == "__main__":
         print(i,j)
 
 
-    #Loss and Val-loss 
+    #Loss and Val-loss
     print('loss sul traing in funzione delle epoche')
-    print(history.history['loss']) 
+    print(history.history['loss'])
     print('\t')
     print('loss sul validation in funzione delle epoche')
-    print(history.history['val_loss']) 
+    print(history.history['val_loss'])
     print('\t')
 
     plt.plot(history.history['loss']) #funzione a gomito dalla quale si deve scegliere il valore ottimale delle epoche
