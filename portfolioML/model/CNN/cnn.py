41d90dc91e18e497c94d6bb1db2701afa1b12ff0
lasala.angelo96@gmail.com <lasala.angelo96@gmail.com>
2021-04-11 20:09:20 +0200

    Update cnn_model fuction with the right argumenta and add the right panser

--- portfolioML/model/CNN/cnn.py
+++ portfolioML/model/CNN/cnn.py
@@ -11,9 +11,9 @@ from keras.utils.vis_utils import plot_model
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath("..")))
-from split import split_Tperiod, get_train_set
-from portfolioML.data.data_returns import read_filepath
-from dnn import all_data_LSTM
+from model.split import split_Tperiod, get_train_set, all_data_LSTM
+from data.data_returns import read_filepath
+from makedir import smart_makedir, go_up
 
 class MinPooling1D(MaxPooling1D):
     """
@@ -30,46 +30,102 @@ class MinPooling1D(MaxPooling1D):
         return -MaxPooling1D(self.pool_size, self.strides, self.padding, self.data_format)(-x)
 
 
-def CNN_model():
+def CNN_model(filters, kernel_size=(20), strides=5, activation='tanh', min_pooling=False, plt_figure=False):
     """
-    CNN model for classification task
-    """
-    # model = Sequential()
-    # model.add(Input(shape=(240,1)))
-    # model.add(Dropout(0.1))
-    # model.add(Conv1D(15, kernel_size=(20), strides=5, activation='tanh'))
-    # model.add(MaxPooling1D(pool_size=5, strides=1, padding='valid'))
-    # model.add(Dropout(0.4))
-    # model.add(Flatten())
-    # model.add(Dropout(0.4))
-    # model.add(Dense(25, activation= 'tanh' ))
-    # model.add(Dropout(0.4))
-    # model.add(Dense(1, activation= 'sigmoid' ))
-    # model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    CNN model with selected number of filters for the convolutional layers for classification
+    task about time-series of data. Beacous of the problem one convolutional layes is enough
 
+    The basic structure is:
+
+    - Inputs: shape=(240,1). The shape reflect the number of close value for each istance (240)
+    and the number of features (1)
+
+    - Dropout: drop(0.1)
+
+    - Convolution: Conv1D(filters, kernel_size=kernel_size, strides=strides, activation='tanh')(drop)
+    The choise of parameters follows the "nature" of the task, the kernel_size is about one mouth 
+    of trading days (20) and the strides is one week (5).
+
+    - Pooling Layers:
+            -- MaxPooling: MaxPooling1D(pool_size=5, strides=1, padding='valid')(conv)
+            find the max value in a pool_size of 5.
+
+            -- If min_pooling = True
+                - MinPooling: MinPooling1D(pool_size=5, strides=1, padding='valid').pooling(conv)
+                find the min value in a pool_size of 5.
+
+                - Merge: Concatenate()([min_pool, max_pool])
+                Merge two pooling layers that have the same input to create a multiheaded structure
+
+    - Dropout: drop(0.1)
+
+    - Dense: Dense(25, activation= 'tanh')(flatten)
+    Compute all the feature and the information of the previous steps
+
+    - Output: Dense(1, activation='sigmoid'), the output is interpretated as the probability that 
+    the input is grater than the cross-section median.
+
+    Parameters
+    ----------
+    filters: integer
+        The dimensionality of the output space (i.e. the number of output filters in the convolution).
+        Referances: https://keras.io/api/layers/convolution_layers/convolution1d/
+
+    kernel_size: tuple of one integers (optional)
+        Specifying the length of the 1D convolution window. Default = (20)
+        Referances: https://keras.io/api/layers/convolution_layers/convolution1d/
+
+    strides: integer
+        specifying the stride length of the convolution. Default = 5
+        Specifying any stride value != 1 is incompatible with specifying any dilation_rate value != 1.
+        Referances: https://keras.io/api/layers/convolution_layers/convolution1d/
+
+    activation: function to use (optional)
+        If you don't specify anything, no activation is applied (see keras.activations). Default = 'tanh'
+        Referances: https://keras.io/api/layers/convolution_layers/convolution1d/
+
+
+    Results
+    -------
+    model: tensorflow.python.keras.engine.sequential.Sequential
+        tensorflow model with selected hidden layers
+    """
     inputs = Input(shape=(240,1))
     drop = Dropout(0.1)(inputs)
-    conv = Conv1D(8, kernel_size=(20), strides=5, activation='tanh')(drop)
-    min_pool = MinPooling1D(pool_size=5, strides=1, padding='valid').pooling(conv)
+
+    conv = Conv1D(filters, kernel_size=kernel_size, strides=strides, activation=activation)(drop)
     max_pool = MaxPooling1D(pool_size=5, strides=1, padding='valid')(conv)
-    merge = Concatenate()([min_pool, max_pool])
-    drop = Dropout(0.4)(merge)
+    if min_pooling:
+        min_pool = MinPooling1D(pool_size=5, strides=1, padding='valid').pooling(conv)
+        merge = Concatenate()([min_pool, max_pool])
+        drop = Dropout(0.4)(merge)
+    else: drop = Dropout(0.4)(max_pool)
+
     flatten = Flatten()(drop)
+
     dense = Dense(25, activation= 'tanh')(flatten)
     outputs = Dense(1, activation='sigmoid')(dense)
 
     model = Model(inputs=inputs, outputs=outputs)
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    plot_model(model, to_file='model_multihead.png', show_shapes=True, show_layer_names=True)
-    print(model.summary())
 
+    if plt_figure:
+        plot_model(model, to_file=f'CNN:fil{filters}_kernel{kernel_size}_strides{strides}_min{min_pooling}.png', 
+                    show_shapes=True, show_layer_names=True)
+
+    logging.info(model.summary())
     return model
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Make CNN model for classification task to predict class label 0 or 1')
-    parser.add_argument('returns_file', type=str, help='Path to the returns input data')
-    parser.add_argument('binary_file', type=str, help='Path to the binary target data')
+    parser.add_argument("filters", type=int, help="filters of convolutional layers")
+    parser.add_argument("-kernel_size", type=tuple, default=(20), help="kernel_size, for more details see documentation")
+    parser.add_argument("-strides", type=int, default=5, help="strides, for more details see documentation")
+    parser.add_argument("-activation", type=str, default='tanh', help="activation, for more details see documentation")
+    parser.add_argument("-min_pooling", type=bool, default=False, help="If true the structure is multiheaded")
+    parser.add_argument("-plt_figure", type=bool, default=False, help="If true create png file of the model")
+
     parser.add_argument("-log", "--log", default="info",
                         help=("Provide logging level. Example --log debug', default='info"))
 
@@ -84,12 +140,14 @@ if __name__ == "__main__":
     logging.basicConfig(level= levels[args.log])
 
     #Read the data
-    df_returns = read_filepath(args.returns_file)
-    df_binary = read_filepath(args.binary_file) 
+    df_binary = go_up(2) + "/data/ReturnsBinary.csv"
+    df_returns = go_up(2) + "/data/ReturnsData.csv"
+    df_returns = read_filepath(df_returns)
+    df_binary = read_filepath(df_binary) 
 
 
     for per in range(0,10):
-        model = CNN_model()
+        model = CNN_model(args.filters, min_pooling=args.min_pooling, plt_figure=args.plt_figure)
         #Training
         X_train, y_train, X_test, y_test = all_data_LSTM(df_returns, df_binary, per)
         #Trainng
