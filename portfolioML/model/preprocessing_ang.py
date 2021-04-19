0ac98277146941be57fc04fda2945a6b085f655a
lasala.angelo96@gmail.com <lasala.angelo96@gmail.com>
2021-04-19 20:29:32 +0200

    Initial import: basic correlation analysis

--- portfolioML/model/preprocessing_ang.py
+++ portfolioML/model/preprocessing_ang.py
@@ -12,13 +12,29 @@ sys.path.append(os.path.dirname(os.path.abspath("..")))
 from portfolioML.data.data_returns import read_filepath
 import matplotlib.pyplot as plt
 import seaborn as sns
+from portfolioML.makedir import smart_makedir, go_up
 
-def stock_features_df(df_returns, time_feature_steps=240):
+def stock_features_df(dataframe, time_feature_steps=5):
     """
     Given a dataset of returns(prices) it returns the feature each time_feature_steps.
     the featare are now only the mean values of returns(prices) each time_feature_steps
+
+    Parameters
+    ----------
+    dataframe: pandas dataframe
+        Input dtaframe
+
+    time_feature_steps: integer (optional)
+        Time window over which compute the feature. Default = 10
+
+    Returns
+    -------
+    data_feature: pandas dataframe
+        Output dataframe. It has gat the companies as istances(records) and the feature
+        as attributes
     """
-    df_feature = [df_returns[i:i+time_feature_steps].mean() for i in range(0, len(df_returns), time_feature_steps)]
+
+    df_feature = [dataframe[i:i+time_feature_steps].mean() for i in range(0, len(dataframe), time_feature_steps)]
     data_feature = pd.DataFrame(df_feature[:-1])
     data_feature = data_feature.transpose()
 
@@ -49,12 +65,12 @@ def correlated_stock(data_feature, anti_value = -0.5, positive_value=0.85):
     anticor_list = []
     for val in anti_cor:
         position = np.where(cor_unique == val)
-        # print(f'value:{val}, position: {position} ... {name[position[0]].values[0]}-{name[position[1]].values}')
+        print(f'value:{val}, position: {position} ... {name[position[0]].values[0]}-{name[position[1]].values}')
         anticor_list.append(name[position[0]].values[0])
         anticor_list.append(name[position[1]].values[0])
 
     anticor_list = list(set(anticor_list))
-    print(f'anti {len(anticor_list)}')
+    print(f'anti {(anticor_list)}')
 
     #positive correlated
     positive_cor = cor_unique[cor_unique > positive_value]
@@ -71,17 +87,15 @@ def correlated_stock(data_feature, anti_value = -0.5, positive_value=0.85):
 
     stocks = list(set(positcor_list + anticor_list))
 
-    return stocks
+    return stocks, cor_np, cormatrix, anticor_list, positcor_list
 
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Creation of input and output data for lstm classification problem')
-    parser.add_argument('returns_file', type=str, help='Path to the returns input data')
-    parser.add_argument('binary_file', type=str, help='Path to the binary target data')
+    parser.add_argument("-time_feature_steps", type=int, default=5, help="Window over which you compute the mean")
     parser.add_argument("-log", "--log", default="info",
                         help=("Provide logging level. Example --log debug', default='info"))
-
     args = parser.parse_args()
 
     levels = {'critical': logging.CRITICAL,
@@ -93,62 +107,37 @@ if __name__ == "__main__":
     logging.basicConfig(level= levels[args.log])
 
     #Read the data
-    df_returns = read_filepath(args.returns_file)
+    df_price = pd.read_csv(go_up(1) + "/data/PriceData.csv")
+    df_price = df_price.dropna(axis=1)
 
     #Create feature
-    df_feature = [df_returns[i:i+240].mean() for i in range(0, len(df_returns), 240)]
-    data_feature = pd.DataFrame(df_feature[:-1])
-    data_feature = data_feature.transpose()
-
-    new_attributes = {i: f'{i+1}-Period' for i in data_feature.columns}
-    data_feature = data_feature.rename(columns=new_attributes)
-
+    data_feature = stock_features_df(df_price[1:], time_feature_steps=args.time_feature_steps)
     print(data_feature.head())
 
     #Correlazioni
-    cormatrix = data_feature.transpose().corr()
-    cor_np = np.array(cormatrix)
-    cor_unique = np.triu(cor_np)
-
-    name = data_feature.transpose().columns
-
-    #anti correlated
-    anti_cor = cor_unique[cor_unique < -0.5]
-    print(len(anti_cor))
-    anticor_list = []
-    for val in anti_cor:
-        position = np.where(cor_unique == val)
-        # print(f'value:{val}, position: {position} ... {name[position[0]].values[0]}-{name[position[1]].values}')
-        anticor_list.append(name[position[0]].values[0])
-        anticor_list.append(name[position[1]].values[0])
-
-    anticor_list = list(set(anticor_list))
-    print(f'anti {len(anticor_list)}')
-
-    #positive correlated
-    positive_cor = cor_unique[cor_unique > 0.75]
-    positive_cor = positive_cor[positive_cor != 1]
-    positcor_list = []
-    for val in positive_cor:
-        position = np.where(cor_unique == val)
-        # print(f'value:{val}, position: {position} ... {name[position[0]].values[0]}-{name[position[1]].values}')
-        positcor_list.append(name[position[0]].values[0])
-        positcor_list.append(name[position[1]].values[0])
-
-    positcor_list = list(set(positcor_list))
-    print(f'posit {len(positcor_list)}')
-
-    data_feature1 = stock_features_df(df_returns, time_feature_steps=240)
-    stocks = correlated_stock(data_feature1)
-    print(len(stocks))
-
-  
+    stocks, cor_np, cormatrix, a, p = correlated_stock(data_feature, anti_value = -0.7, positive_value=0.95)
+    print(f'selected stocks:{len(stocks)}')
 
     plt.figure()
     plt.imshow(np.triu(cor_np))
 
     ax = plt.figure()
     ax = sns.heatmap(cormatrix, cmap=sns.diverging_palette(20, 220, n=200))
+
+    #Sub matrix
+    stocks.sort()
+    cormatrix_sub = cormatrix[stocks].loc[stocks] 
+    positive = cormatrix[cormatrix > 0.9][p].loc[p]
+    negative = cormatrix[cormatrix < -0.7][a].loc[a]    
+
+    ax = plt.figure()
+    ax = sns.heatmap(cormatrix_sub, cmap=sns.diverging_palette(20, 220, n=200))
+
+    plt.figure()
+    plt.imshow(positive)
+    plt.imshow(negative)
+    plt.colorbar()
+
     plt.show()
     
 
