cc33d5ea4bd6f436adfa4b6ed4c16605a4fbc32d
DanieleMDiNosse <danielemdinosse@gmail.com>
2021-04-21 09:31:49 +0200

    preprocessing with pca and DWT

--- portfolioML/data/preprocessing.py
+++ portfolioML/data/preprocessing.py
@@ -2,9 +2,11 @@ import argparse
 import logging
 import os
 import numpy as np
+import pandas as pd
 from sklearn.decomposition import PCA
 from portfolioML.data.data_returns import read_filepath
 from portfolioML.data.wavelet import approx_details_scale
+from portfolioML.model.split import all_data_LSTM
 
 
 def pca(df_returns_path, n_components):
@@ -34,37 +36,45 @@ def pca(df_returns_path, n_components):
     most_important = [np.abs(pca.components_[i]).argmax() for i in range(n_pca)] # get the index of the most important feature on EACH component
     initial_feature_names = df_returns.columns
     most_important_features = [initial_feature_names[most_important[i]] for i in range(n_pca)] # get the most important feature names
-    most_important_features = list(set(most_important_companies))
+    most_important_features = list(set(most_important_features))
 
     return most_important_features
 
-    def wavelet_dataframe(df_price_path, wavelet):
-        '''
-        Compute the DWT (Discrete Wavelet Tranform) of a dataset composed by multiple time signals
+def wavelet_dataframe(df_returns_path, wavelet):
+    '''
+    Compute the DWT (Discrete Wavelet Tranform) of a dataset composed by multiple time signals
 
-        Parameters
-        ----------
-        df_price_path : str
-            Path of the pandas dataframe of time signals
-        wavelet : str
-            Which wavelet is used during the decomposition.
+    Parameters
+    ----------
+    df_price_path : str
+        Path of the pandas dataframe of time signals
+    wavelet : str
+        Which wavelet is used during the decomposition.
 
-        Returns
-        -------
-            d : dictionary
-        '''
+    Returns
+    -------
+        dataframe : pandas.DataFrame
+            Pandas dataframe in which each element is composed by the three timestamp of the first 3 approximations
+    '''
 
-        df_price = read_filepath(df_price_path)
-        df_price = df_price.dropna(axis=1) # Drop NaN
-        df_price = df_price.drop(labels='Date', axis=1)
-        d = {}
-        for tick in df_price.columns[:5]:
-            a1,d1 = approx_details_scale(df_price[tick], wavelet, 1)
-            a2,d2 = approx_details_scale(df_price[tick], wavelet, 2)
-            a3,d3 = approx_details_scale(df_price[tick], wavelet, 3)
-            d[tick] = [[a1[i], a2[i], a3[i]] for i in range(len(a1))]
+    # df_price = pd.read_csv(df_price_path)
+    # df_price = df_price.dropna(axis=1) # Drop NaN
+    # df_price = df_price.drop(labels='Date', axis=1)
+    df_returns = read_filepath(df_returns_path)
+    dic1, dic2, dic3 = {}, {}, {}
+    for tick in df_returns.columns:
+        a1,d1 = approx_details_scale(df_returns[tick], wavelet, 1)
+        a2,d2 = approx_details_scale(df_returns[tick], wavelet, 2)
+        a3,d3 = approx_details_scale(df_returns[tick], wavelet, 3)
+        dic1[tick], dic2[tick], dic3[tick] = a1, a2, a3
+    dataframe1 = pd.DataFrame(dic1)
+    dataframe2 = pd.DataFrame(dic2)
+    dataframe3 = pd.DataFrame(dic3)
+    dataframe1.to_csv("MultidimReturnsData1.csv")
+    dataframe2.to_csv("MultidimReturnsData2.csv")
+    dataframe3.to_csv("MultidimReturnsData3.csv")
 
-        return d
+    return dataframe1, dataframe2, dataframe3
 
 
 if __name__ == "__main__":
@@ -83,6 +93,11 @@ if __name__ == "__main__":
 
 
     df_returns_path = os.getcwd() + "/ReturnsData.csv"
-    df_price_path = od.getcwd() + "/PriceData.csv"
-    most_important_companies = pca(df_returns_path)
-    wavelet_data = wavelet_dataframe(df_price_path, 'haar')
+    df_binary = read_filepath("ReturnsBinary.csv")
+    # most_important_companies = pca(df_returns_path, n_components=250)
+    wavelet_data1, wavelet_data2, wavelet_data3 = wavelet_dataframe(df_returns_path, 'haar')
+    # X_train1, y_train, X_test1, y_test = all_data_LSTM(wavelet_data1, df_binary, 1)
+    # X_train2, y_train, X_test2, y_test = all_data_LSTM(wavelet_data2, df_binary, 1)
+    # X_train3, y_train, X_test3, y_test = all_data_LSTM(wavelet_data3, df_binary, 1)
+    # X_train = np.stack((X_train1, X_train2, X_train3), axis=-1).reshape(X_train1.shape[0],240,3)
+    # X_test = np.stack((X_test1, X_test2, X_test3), axis=-1).reshape(X_test1.shape[0],240,3)
