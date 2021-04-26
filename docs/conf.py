aeca41013fcc998eba3672a2e2e28a1beddcabde
Daniele Maria Di Nosse <72073641+DanieleMDiNosse@users.noreply.github.com>
2021-04-26 15:35:31 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+autodoc_mock_imports = ["matplotlib"]
 
 
 # -- Options for HTML output -------------------------------------------------
