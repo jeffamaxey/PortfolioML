1dd5debaec4c352102c96c33b69c5c9438c733ed
DanieleMDiNosse <danielemdinosse@gmail.com>
2021-04-27 16:35:23 +0200

    typo

--- portfolioML/tests/test_portfolio_creation.py
+++ portfolioML/tests/test_portfolio_creation.py
@@ -7,6 +7,9 @@ import numpy as np
 import pandas as pd
 from portfolioML.makedir import go_up
 
+def _full_path(file_name):
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), file_name)
+
 
 class TestDataReturns(unittest.TestCase):
 
@@ -17,7 +20,7 @@ class TestDataReturns(unittest.TestCase):
         rand1 = random.randint(0, 3)
         algorithm = ['CNN', 'RAF', 'LSTM', 'DNN']
         model_name = ['CNN_dense', 'RAF_model1', 'LSTM_Model3', 'DNN_paper']
-        trading_days = pd.read_csv(go_up(1) + f'''/predictions_for_portfolio/
+        trading_days = pd.read_csv(f'''portfolioML/results/predictions_for_portfolio/
                         {algorithm[rand1]}/{model_name[rand1]}/Trading_days_period{rand}.csv''')
         first_column = trading_days.columns[0]
         self.assertEqual(first_column, 'Date')
