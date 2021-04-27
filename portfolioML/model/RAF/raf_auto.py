8acbcdc36c2224b5fe333d9c8367dc8de83ca21e
RaffaeleParadiso <raffaele05@gmail.com>
2021-04-27 14:00:46 +0200

    RAF_auto predictions

---
+++ portfolioML/model/RAF/raf_auto.py
@@ -0,0 +1,113 @@
+'''Random Forest'''
+import argparse
+import logging
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score,roc_curve
+from portfolioML.model.split import all_data_DNN
+from portfolioML.makedir import smart_makedir, go_up
+from portfolioML.data.preprocessing import pca
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
+
+
+def predictions_roc(n_estimators, max_depth, num_period, criterion):
+    path_n = go_up(level_up=2)
+    path_p = path_n + f'/results/predictions/RAF/{args.name_model}/'
+    path_r = path_n + f'/results/ROC/RAF/{args.name_model}/'
+
+    tpr_list = []
+    aucs_list = []
+    interp_fpr = np.linspace(0, 1, 10000)
+    plt.figure()
+
+    x_train, y_train, x_test, y_test = all_data_DNN(df_returns, df_binary, 0)
+
+    rf_ = RandomForestClassifier(n_estimators, criterion, max_depth, n_jobs=-1,
+                                min_samples_split=2, min_samples_leaf=1,
+                                random_state=10, verbose=1)
+    rf_.fit(x_train_auto, y_train)
+    y_proba = rf_.predict_proba(x_test_auto)
+
+
+    fpr, tpr, thresholds = roc_curve(y_test, y_proba[:,1])
+    interp_tpr = np.interp(interp_fpr, fpr, tpr)
+    tpr_list.append(interp_tpr)
+    roc_auc = roc_auc_score(y_test, y_proba[:,1])
+    aucs_list.append(roc_auc)
+
+    plt.plot(fpr, tpr, label=f'per{0} (area = %0.4f)' % (roc_auc))
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlabel('False Positive Rate',)
+    plt.ylabel('True Positive Rate')
+    plt.legend(loc="lower right", fontsize=12, frameon=False)
+    plt.title(f'ROC CURVE')
+    plt.savefig(path_r + f'ROC_CURVE.png')
+
+    y_pred_companies = [y_proba[i:87+i] for i in range(0,len(y_proba)-87+1,87)]
+    dict_comp = {df_returns.columns[i]: y_pred_companies[i] for i in range(len(df_returns.columns))}
+    df_predictions = pd.DataFrame()
+    for tick in df_returns.columns:
+        df_predictions[tick] = dict_comp[tick][:,0]
+        df_predictions.to_csv(path_p + f'RAF_{args.name_model}_Predictions_{0}th_Period.csv')
+
+    auc_mean = np.mean(np.array(aucs_list))
+    auc_std = np.std(np.array(aucs_list))
+    tpr_mean = np.mean(tpr_list, axis=0)
+
+    plt.figure()
+    plt.plot(interp_fpr, tpr_mean, color='b',
+            label=fr'Mean ROC (AUC = {auc_mean:.4f} $\pm$ {auc_std:.4f})', lw=1, alpha=.8)
+    tpr_std = np.std(tpr_list, axis=0)
+    tprs_upper = np.minimum(tpr_mean + tpr_std, 1)
+    tprs_lower = np.maximum(tpr_mean - tpr_std, 0)
+    plt.fill_between(interp_fpr, tprs_lower, tprs_upper, color='blue', alpha=.2,
+                    label=r'$\pm$ 1 std. dev.')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.legend(loc="lower right", fontsize=12, frameon=False)
+    plt.title(f'ROC Curve - mean +|- std')
+    plt.savefig(path_r + f'ROC Curve - mean +|- std')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='RandomForestClassifier')
+    parser.add_argument("-log", "--log", default="info",
+                        help=("Provide logging level. Example --log debug', default='info"))
+    parser.add_argument('-num_period', type=int, default=10, help='Number of periods (default=10)')
+    parser.add_argument('-n_estimators', type=int, default=1000, help='Number of trees (default=1000)')
+    parser.add_argument('-criterion', type=str, default='gini', help='Criterion (default=gini)')
+    parser.add_argument('-max_depth', type=int, default=25, help='Trees\'s depth (default=25) ')
+    parser.add_argument('name_model', type=str, help='Name_model')
+    args = parser.parse_args()
+
+    levels = {'critical': logging.CRITICAL,
+              'error': logging.ERROR,
+              'warning': logging.WARNING,
+              'info': logging.INFO,
+              'debug': logging.DEBUG}
+
+    logging.basicConfig(level= levels[args.log])
+    pd.options.mode.chained_assignment = None
+
+    df_returns_path = go_up(2) + "/data/ReturnsDataPCA.csv"
+    df_binary_path = go_up(2) + "/data/ReturnsBinaryPCA.csv"
+    df_auto_train_path = go_up(2) + "/data/after_train.csv"
+    df_auto_test_path = go_up(2) + "/data/after_test.csv"
+
+    df_returns = pd.read_csv(df_returns_path)
+    df_binary = pd.read_csv(df_binary_path)
+    df_auto_train = pd.read_csv(df_auto_train_path, index_col=0)
+    df_auto_test = pd.read_csv(df_auto_test_path, index_col=0)
+
+    x_train_auto = np.array(df_auto_train)
+    x_test_auto = np.array(df_auto_test)
+    smart_makedir(f'/results/predictions/RAF/{args.name_model}/', level_up=2)
+    smart_makedir(f'/results/ROC/RAF/{args.name_model}/', level_up=2)
+
+    predictions_roc(n_estimators=args.n_estimators, max_depth=args.max_depth, num_period=args.num_period, criterion=args.criterion)
+    with open(f"{args.name_model}.txt", 'a', encoding='utf-8') as file:
+        file.write(f'num_period={args.num_period}, n_estimators={args.n_estimators}, criterion={args.criterion},max_depth={args.max_depth}' + '\n')
