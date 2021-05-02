49f4d99810204ffd19c1edba3b3b2f82e55384b8
RaffaeleParadiso <raffaele05@gmail.com>
2021-05-02 16:22:50 +0200

    changes

--- portfolioML/data/autoencoders.py
+++ portfolioML/data/autoencoders.py
@@ -1,5 +1,8 @@
 '''
-Autoencoder
+Autoencoder for features extraction
+N.B. this algorithm takes a lot of time (20 hours) just only for one period.
+It was not worth it to implement for every periods of study (17 periods).
+
 '''
 import argparse
 import logging
@@ -9,15 +12,22 @@ import numpy as np
 import pandas as pd
 from tensorflow.keras import regularizers
 from tensorflow.keras.layers import Input, Dense
-from tensorflow.keras.models import Model
+from tensorflow.keras.models import Model, load_model, save_model
 from tensorflow.keras.utils import plot_model
 from portfolioML.model.split import all_data_LSTM
 from portfolioML.makedir import smart_makedir
-from portfolioML.data.preprocessing import pca
 
-def autoencoder(df_returns, df_binary, period, bottneck):
+def autoencoder(df_returns, df_binary, period, bottneck, save=True, plot=False):
     """
     Creation of a csv file with features selected from autoencoder.
+
+    An autoencoder is a neural network that is trained to attempt to copy its input to its output.
+    Usually they are restricted in a ways that allow them to copy only approximately
+    and so the model is forced to prioritize which aspects of the input should be copied.
+    One way to obtain useful features from the autoencoder is to constrain the middle layer
+    to have smaller dimension than the input layer.
+    An autoencoder whit this property is called undercomplete.
+    The learning process is performed minimizing a loss function (like mean squared error).
 
     Parameters
     ----------
@@ -39,82 +49,88 @@ def autoencoder(df_returns, df_binary, period, bottneck):
         Csv file with selected features extract from encoder.
     """
 
+    smart_makedir(f'Autoencoder/autoencoder_period_{period}/')
+
     x_train, y_train, x_test, y_test = all_data_LSTM(
         df_returns, df_binary, period)
     x_train = np.reshape(x_train, (len(x_train), 1, 240))
     print(len(x_train))
 
-    input_img = Input(shape=(240,))
+    input_img = Input(shape=(240,), name='Input')
     encoded = Dense(150, activation='relu',
-                    activity_regularizer=regularizers.l1(10e-5))(input_img)
+                    activity_regularizer=regularizers.l1(10e-5), name='dense1')(input_img)
     encoded = Dense(80, activation='relu',
-                    activity_regularizer=regularizers.l1(10e-5))(encoded)
-    bottleneck = Dense(bottneck)(encoded)
-    decoder = Dense(80, activation='relu')(bottleneck)
-    decoded = Dense(150, activation='relu')(decoder)
-    decoded = Dense(240, activation='linear')(decoded)
+                    activity_regularizer=regularizers.l1(10e-5), name='dense2')(encoded)
+    bottleneck = Dense(bottneck, name='MiddleLayer')(encoded)
+    decoder = Dense(80, activation='relu', name='dense3')(bottleneck)
+    decoded = Dense(150, activation='relu', name='dense4')(decoder)
+    decoded = Dense(240, activation='linear', name='dense5')(decoded)
 
     start = time.time()
-
-    lenght_x_train = len(x_train)
     after = []
-    for i in range(lenght_x_train):
+    for i in range(5,6):
         encoder = Model(input_img, bottleneck)
         encoder.compile(optimizer='adam', loss='mse')
         autoencod = Model(input_img, decoded)
         autoencod.compile(optimizer='adam', loss='mse')
-        history = autoencod.fit(
-            x_train[i], x_train[i], epochs=40, batch_size=1, shuffle=False)
-        # autoencod.summary()
+
+        history = autoencod.fit(x_train[i], x_train[i], epochs=40, batch_size=1, shuffle=False)
+        autoencod.summary()
         reconstructed_data = autoencod.predict(x_train[i])
         afterbot = encoder.predict(x_train[i])
         after.append(afterbot[0])
 
         difference = x_train[i] - reconstructed_data
 
-        # plt.figure(f'selected feature {i}')
-        # plt.subplot(131)
-        # plt.bar(list(range(len(x_train[0][0]))), x_train[0][0], color='b', alpha=0.5)
-        # plt.bar(list(range(len(reconstructed_data[0]))), reconstructed_data[0], color='r', alpha=0.5)
-        # plt.subplot(132)
-        # plt.bar(list(range(len(difference[0]))),difference[0], color='orange', alpha=0.5)
-        # plt.subplot(133)
-        # plt.bar(list(range(len(afterbot[0]))), afterbot[0], color='green')
-
+        if plot:
+            plt.figure(f'selected feature {i}', figsize=[18, 5])
+            plt.subplot(131)
+            plt.bar(list(range(len(x_train[0][0]))), x_train[0][0], color='darkgreen')
+            plt.xlabel('Features')
+            plt.title('Original sequence')
+            plt.subplot(132)
+            plt.bar(list(range(len(afterbot[0]))), afterbot[0], color='cornflowerblue', alpha=0.8)
+            plt.title('Encoded sequence')
+            plt.xlabel('Features')
+            plt.subplot(133)
+            plt.bar(list(range(len(reconstructed_data[0]))), reconstructed_data[0],\
+                                    color='forestgreen', label='reconstructed')
+            plt.bar(list(range(len(difference[0]))),difference[0], color='crimson', label='differences')
+            plt.title('Reconstructed sequence')
+            plt.xlabel('Features')
+            plt.legend()
+            plt.savefig('autoencoder.png')
+    
     after = np.array(after)
     df = pd.DataFrame(
         after, columns=[f'selected feature_{i}' for i in range(len(after[0]))])
-    df.to_csv('encoder_train.csv', index=False)
+    df.to_csv(f'Autoencoder/autoencoder_period_{period}/encoder_train.csv', index=False)
 
     end = time.time()
     print(f'Total time: {end-start} seconds')
-    # dot_img_autoencod = f'Autoencoder_for_period_{period}/autoencoder_nocompress_for_period_{period}.png'
-    # plot_model(autoencod, to_file=dot_img_autoencod, show_shapes=True)
-    # dot_img_encoder = f'Autoencoder_for_period_{period}/encoder_nocompress_for_period_{period}.png'
-    # plot_model(encoder, to_file=dot_img_encoder, show_shapes=True)
-    # encoder.save(f'Autoencoder_for_period_{period}/encoder_{period}.h5')
-    # encoder = load_model('Autoencoder/encoder.h5')
 
-    # plt.figure()
-    # plt.bar(list(range(len(difference[0]))), difference[0])
-    # plt.figure()
-    # plt.plot(history.history['loss'], label='train')
-    # plt.plot(history.history['val_loss'], label='test')
-    # plt.legend()
-    # plt.show()
+    dot_img_auto = f'Autoencoder/autoencoder_period_{period}/autoencoder_model_train.png'
+    plot_model(autoencod, to_file=dot_img_auto, show_shapes=True)
+    dot_img_enco = f'Autoencoder/autoencoder_period_{period}/encoder_model_train.png'
+    plot_model(encoder, to_file=dot_img_enco, show_shapes=True)
 
-    return after
+    if save:
+        autoencod.save(f'Autoencoder/autoencoder_period_{period}/autoencoder_train.h5')
+        encoder.save(f'Autoencoder/autoencoder_period_{period}/encoder_train.h5')
+    
+    plt.show()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Creation of encoder for data reduction and features selection.')
+                    description='Creation of encoder for data reduction and features selection.')
     requiredNamed = parser.add_argument_group('Required named arguments')
-    requiredNamed.add_argument(
-        '-r', '--returns_file', type=str, help='Path to the returns data.')
-    requiredNamed.add_argument(
-        '-b', '--binary_file', type=str, help='Path to the binary target data.')
-    requiredNamed.add_argument('-p', '--period', type=int, help='Study period.')
+    requiredNamed.add_argument('-p', '--period', type=int,
+                        help='Study period.')
+    requiredNamed.add_argument('-l', '--load', action='store_true',
+                        help='Type -l to load the encoder.')
+    requiredNamed.add_argument('-c', '--create', action='store_true',
+                        help='Type -c to create the encoder.')
     parser.add_argument('-bn', '--botneck', type=int, default=31,
                         help='Number of nodes in the middle layer (default=31).')
     parser.add_argument("-log", "--log", default="error",
@@ -130,9 +146,26 @@ if __name__ == "__main__":
     logging.basicConfig(level=levels[args.log])
     pd.options.mode.chained_assignment = None
 
-    df_return = pd.read_csv(args.returns_file)
-    df_binary = pd.read_csv(args.binary_file)
+    df_ret = pd.read_csv('ReturnsDataPCA.csv')
+    df_bin = pd.read_csv('ReturnsBinaryPCA.csv')
     per = args.period
     botneck = args.botneck
 
-    autoencoder(df_return, df_binary, per, botneck)
+    loaded = args.load
+    created = args.create
+
+    if loaded:
+        encoder = load_model(f'Autoencoder/autoencoder_period_{per}/encoder_train.h5')
+        x_train, y_train, x_test, y_test = all_data_LSTM(df_ret, df_bin, per)
+        x_train = np.reshape(x_train, (len(x_train), 1, 240))
+        after = []
+        for i in range(len(x_train)):
+            afterbot = encoder.predict(x_train[i])
+            after.append(afterbot[0])
+        after = np.array(after)
+        df = pd.DataFrame(
+        after, columns=[f'selected feature_{i}' for i in range(len(after[0]))])
+        df.to_csv('encoder_train123.csv', index=False)
+
+    if created:
+        autoencoder(df_ret, df_bin, per, botneck)
